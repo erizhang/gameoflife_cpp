47ed07bd7ee28230867254c7d8718af5a2d17923
Eric Zhang <zhang.lyuan@gmail.com>
2013-09-30 13:23:04 +0800

    build up the source code structure: main.cpp, makefile etc.

--- include/Game.h
+++ include/Game.h
@@ -1,8 +1,15 @@
 
+#include <string>
+
+using namespace std;
 
 Class Game{
-
-
+public:
+    Game(int w, int h, String layout);
+    ~Game();
+private:
+    int width;
+    int height;
 };
 
 
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Start Game ..." <<endl;
+    return 0;
+}
